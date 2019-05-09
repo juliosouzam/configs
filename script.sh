57c61fcbcae142b933891d5035d50023bca271c7
Júlio César <julio.souzam@gmail.com>
2019-05-09 17:21:20 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-yay -S php composer docker docker-compose visual-studio-code-bin nodejs npm slack-desktop jetbrains-toolbox
+yay -S php php-gd php-intl php-sqlite php-xsl composer docker docker-compose visual-studio-code-bin nodejs npm slack-desktop jetbrains-toolbox postman-bin phpunit
