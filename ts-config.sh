54f4a3774f4c6c90f218bde9ea2c2f37eb6b201d
Júlio César <julio.souzam@gmail.com>
2019-10-05 10:22:00 -0300

    Update ts-config.sh

--- ts-config.sh
+++ ts-config.sh
@@ -2,4 +2,4 @@
 
 yarn add typescript sucrase express cors mongoose
 
-yarn add -D nodemon eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin @types/express @types/cors @types/mongoose
+yarn add -D nodemon eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin @types/express @types/cors @types/mongoose prettier eslint-config-prettier eslint-plugin-prettier
