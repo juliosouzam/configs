2d3591f9fc7bbe0e23937e7ccf6ae65736e3db48
Júlio César <julio.souzam@gmail.com>
2019-10-04 23:19:55 -0300

    Create ts-config.sh

---
+++ ts-config.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+yarn add typescript sucrase express cors mongoose
+
+yarn add -D nodemon eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin @types/express @types/cors @types/mongoose
