2a10a8f89ec4b9048221b18bd592715c2189f5a7
Julio Cesar <julio.souzam@gmail.com>
2020-01-13 14:45:12 -0300

    update and add config files vscode

---
+++ post_installer.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+deps = ark android-tools docker docker-compose firefox lm_sensors net-tools nvm yarn vlc vim yakuake
--- script.sh
+++
@@ -1,3 +0,0 @@
-#/bin/bash
-
-yay -S php php-gd php-intl php-sqlite php-xsl composer docker docker-compose visual-studio-code-bin nodejs npm slack-desktop jetbrains-toolbox postman-bin phpunit
