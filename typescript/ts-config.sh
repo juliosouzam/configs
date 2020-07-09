bde5e4b5bfed726b55c2835d9ad27521757236a1
Júlio César <julio.souzam@gmail.com>
2020-07-08 23:14:19 -0300

    Delete ts-config.sh

--- typescript/ts-config.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-
-yarn add typescript sucrase express cors mongoose
-
-yarn add -D nodemon eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin @types/express @types/cors @types/mongoose prettier eslint-config-prettier eslint-plugin-prettier
